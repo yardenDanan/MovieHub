d2e41fb4ad4ab84febd97fbbf81f62f6f3b0ae13
yardenDanan <34451733+yardenDanan@users.noreply.github.com>
2018-11-17 19:16:40 +0200

    add message button

--- MovieGuide-master/app/src/main/java/com/YardenAvihai/moviehub/details/MovieDetailsActivity.java
+++ MovieGuide-master/app/src/main/java/com/YardenAvihai/moviehub/details/MovieDetailsActivity.java
@@ -1,13 +1,18 @@
 package com.YardenAvihai.moviehub.details;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.YardenAvihai.moviehub.R;
 import com.YardenAvihai.moviehub.Constants;
 import com.YardenAvihai.moviehub.Movie;
+import com.YardenAvihai.moviehub.SignupActivity;
 
 public class MovieDetailsActivity extends AppCompatActivity
 {
@@ -41,5 +46,10 @@ public class MovieDetailsActivity extends AppCompatActivity
                 NavUtils.navigateUpFromSameTask(this);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void messageButton_click(View view) {
+        Toast.makeText(getApplicationContext(), "Button clicked!", Toast.LENGTH_SHORT).show();
+
     }
 }
